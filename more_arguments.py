11ead2d21d1cd9cde5803de4379212494c82162d
Alice <arnoldinwonderland@gmail.com>
2019-02-20 19:49:11 +0000

    woo changes!

---
+++ more_arguments.py
@@ -0,0 +1,12 @@
+def add_two_numbers():
+    number1 = 1
+    number2 = 2
+    answer = number1+number2
+    print( "{} plus {} is {}".format(number1,number2,answer))
+
+add_two_numbers()
+
+def greetings_from_collaborator(name):
+    print("Hi, I am your collaborator, {} :)".format(name))
+
+greetings_from_collaborator("Gloria")
